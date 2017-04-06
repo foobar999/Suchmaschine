9eecc6f7c12ac53cc9348fdb2cadafb392396dc5
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-06 18:38:38 +0200

    extracted method _generate_nested_tuple_list()

--- src/boolean_query_parser.py
+++ src/boolean_query_parser.py
@@ -10,17 +10,7 @@ class BooleanQueryParser(object):
     LBRACKET = '('
     RBRACKET = ')'
 
-    def parse_dnf_query(self, query):  
-        logging.info('parsing query: ' + query)  
-        
-        if self.AND in query and self.OR not in query:
-            query = self.LBRACKET + query + self.RBRACKET
-            logging.debug('query has 1+ ANDs, 0 ORs -> added brackets: ' + str(query))  
-        
-        query_toks = re.split('(\(|\)| )', query)
-        query_toks = list(filter(str.strip, query_toks))
-        logging.debug('parsed query tokens: ' + str(query_toks))  
-        
+    def _generate_nested_tuple_list(self, query_toks):
         outer_list = []
         inner_list = None
         current_list = outer_list
@@ -33,10 +23,25 @@ class BooleanQueryParser(object):
                 outer_list.append(inner_list)
                 current_list = outer_list
             elif self._is_word(tok):
-                is_pos_literal = (i == 0 or query_toks[i-1] != self.NOT)
+                is_pos_literal = i == 0 or query_toks[i - 1] != self.NOT
                 current_list.append((tok, is_pos_literal))
+                
+        return outer_list
+    
+
+    def parse_dnf_query(self, query):  
+        logging.info('parsing query: ' + query)  
         
-        return outer_list     
+        if self.AND in query and self.OR not in query:
+            query = self.LBRACKET + query + self.RBRACKET
+            logging.debug('query has 1+ ANDs, 0 ORs -> added brackets: ' + str(query))  
+        
+        query_toks = re.split('(\(|\)| )', query)
+        query_toks = list(filter(str.strip, query_toks))
+        logging.debug('parsed query tokens: ' + str(query_toks))  
+        
+        return self._generate_nested_tuple_list(query_toks)
+     
     
     def _is_word(self, token):
         return token not in (self.AND, self.OR, self.NOT, self.LBRACKET, self.RBRACKET)
