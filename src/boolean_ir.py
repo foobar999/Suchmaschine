d3934a6bd767323610bccb26a210be711c236f5c
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-07 12:59:52 +0200

    added boolean ir complement

--- src/boolean_ir.py
+++ src/boolean_ir.py
@@ -1,7 +1,9 @@
+import logging
 
 class BooleanIR(object):
 
     def intersect(self, posting1, posting2):
+        logging.debug("intersect of {}, {}".format(posting1, posting2))
         res = []
         i1, i2 = 0, 0
         while i1 < len(posting1) and i2 < len(posting2):
@@ -17,6 +19,7 @@ class BooleanIR(object):
         return res
     
     def union(self, posting1, posting2):
+        logging.debug("union of {}, {}".format(posting1, posting2))
         res = []
         i1, i2 = 0, 0
         while i1 < len(posting1) and i2 < len(posting2):
@@ -33,3 +36,17 @@ class BooleanIR(object):
                 i2 += 1
         # hänge den Rest der Liste an, bei der i nicht am Ende ist
         return res + posting1[i1:] + posting2[i2:]
+    
+    def complement(self, posting, universe):
+        logging.debug("complement of {}, universe {}".format(posting, universe))
+        res = []
+        ip, iu = 0, 0
+        while ip < len(posting):
+            docp, docu = posting[ip], universe[iu]
+            if docu == docp:
+                ip += 1
+                iu += 1
+            if docu < docp:
+                res.append(docu)
+                iu += 1
+        return res + universe[iu:]
