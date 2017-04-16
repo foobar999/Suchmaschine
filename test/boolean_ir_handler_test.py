069cbd9fb5f9c1ccfeee2ccf9c5e39695d81f614
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-16 09:31:07 +0200

    refactored boolean ir handler tests

--- test/boolean_ir_handler_test.py
+++ test/boolean_ir_handler_test.py
@@ -2,27 +2,29 @@
 import unittest
 from os import path, getcwd, pardir
 from src.index_builder import IndexBuilder
-from boolean_ir_handler import BooleanIRHandler
+from src.boolean_ir_handler import BooleanIRHandler
 
 class BooleanIRHandlerTest(unittest.TestCase):
 
-    def test_queries(self):
+    @classmethod
+    def setUpClass(cls):
         data_folder = path.join(path.join(getcwd(), pardir), "data")
-        index, docs_dict = IndexBuilder().build_from_folder(data_folder)
-        handler = BooleanIRHandler()
-        queries = [
-            'hexe',
-            'hexe AND prinzessin',
-            '(hexe AND prinzessin) OR (frosch AND könig AND tellerlein)',
-            '(hexe AND prinzessin) OR (NOT hexe AND könig)'
-            ]
-        results = [
-            [6, 10, 14],
-            [],
-            [6],
-            [0, 2, 3, 4, 7, 9, 15, 18, 21, 22, 25]
-            ]
-        for q, res in zip(queries, results):
-            handler_res = handler.handle_query(q, index, docs_dict)
-            doc_ids = [entry.docID for entry in handler_res]
-            self.assertEqual(res, doc_ids)
+        cls.index, cls.docs_dict = IndexBuilder().build_from_folder(data_folder)
+        cls.handler = BooleanIRHandler()
+        
+    def _test_query(self, expected_res, q):
+        res = [entry.docID for entry in self.handler.handle_query(q, self.index, self.docs_dict)]
+        self.assertEqual(expected_res, res)
+        
+    def test_0_operators(self):
+        self._test_query([6, 10, 14], 'hexe')
+        
+    def test_1_and(self):
+        self._test_query([], 'hexe AND prinzessin')
+        
+    def test_3_ands_1_or(self):
+        self._test_query([6], '(hexe AND prinzessin) OR (frosch AND könig AND tellerlein)')
+        
+    def test_1_not_2_ands_1_or(self):
+        self._test_query([0, 2, 3, 4, 7, 9, 15, 18, 21, 22, 25], '(hexe AND prinzessin) OR (NOT hexe AND könig)')
+        
