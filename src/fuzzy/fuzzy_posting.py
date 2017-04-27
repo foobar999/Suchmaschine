7115f67fc4ddfe442313747052af36c509ba342d
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-27 21:49:18 +0200

    added fuzzy posting repr

--- src/fuzzy/fuzzy_posting.py
+++ src/fuzzy/fuzzy_posting.py
@@ -6,3 +6,6 @@ class FuzzyPosting(Posting):
     def __init__(self, docID, mem_val):
         super().__init__(docID, None)
         self.mem_val = mem_val
+        
+    def __repr__(self):
+        return '{}({},{})' .format(type(self).__name__, str(self.docID), self.mem_val)
