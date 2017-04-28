4c152a55579aae7a482d3885c57ca16e07364f6b
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-04-28 16:29:08 +0200

    added 3 and tests

--- test/fuzzy_ir_handler_test.py
+++ test/fuzzy_ir_handler_test.py
@@ -14,7 +14,7 @@ class FuzzyIRHandlerTest(unittest.TestCase):
         
     def _test_query(self, expected_res, query):
         res = self.hdl.handle_query(query, self.index, self.doc_ids)
-        tuple_res = [(post.docID, post.mem_val) for post in res]
+        tuple_res = [(post.docID, round(post.mem_val, 2)) for post in res]
         self.assertEqual(expected_res, tuple_res)
         
     def test_a(self):
@@ -31,4 +31,15 @@ class FuzzyIRHandlerTest(unittest.TestCase):
         
     def test_not_a_or_b_or_not_c(self):
         self._test_query([(0,1), (1,0.99), (2,1), (3,0.4), (4,1), (5,1)], 'NOT a OR b OR NOT c')
+        
+    def test_a_and_b_and_c(self):
+        self._test_query([], 'a AND b AND c')
+        
+    def test_not_a_and_not_b_and_not_c(self):
+        self._test_query([(0,1), (2,0.2), (3,0.25), (5,1)], 'NOT a AND NOT b AND NOT c')
+        
+    def test_not_b_c_not_a(self):
+        self._test_query([(3,0.4)], 'NOT b AND c AND NOT a')
+        
+        
         
