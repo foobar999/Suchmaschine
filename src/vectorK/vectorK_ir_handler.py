c2ff80170f78adcace3563d69bc45512b1f70083
LdPx <tilman.schramke@stud.hn.de>
2017-05-24 09:32:57 +0200

    ok.

--- src/vectorK/vectorK_ir_handler.py
+++ src/vectorK/vectorK_ir_handler.py
@@ -3,7 +3,7 @@ import logging
 
 class VectorKIRHandler(object):
     
-    def handle_query(self, query, index, numdocs):
+    def handle_query(self, query, cluster, index, numdocs):
         logging.debug('handling query {}'.format(query))
         query_terms = [tok.lower() for tok in query.split()]
         logging.debug('split query to {}'.format(query_terms))
@@ -11,5 +11,19 @@ class VectorKIRHandler(object):
         # TODO
         # dist Query <=> Leaders
             # dist Query <=> Followers (of selected leaders)
+            
+        #=======================================================================
+        # Ok. I have no idea how to do this.
+        #
+        # for pos in range(0, len(terms)):
+        #     t = Term(terms[pos])
+        #     if t not in index:
+        #         index[t] = TermPostings()
+        #     index[t].postings.append(Posting(docID, positions_of_term[t]))
+        #                 
+        #=======================================================================
+            
+            
+            
             
         return -1
